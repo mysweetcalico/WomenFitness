686c6db716635bdcfcaa9090315a54f0da431e1b
angela-young <60243824+angela-young@users.noreply.github.com>
2020-10-09 03:26:07 -0400

    Fixes

--- public/main.js
+++ public/main.js
@@ -4,6 +4,8 @@ var trainerApp = document.getElementById("trainerApp");
 function nutritionistAppDisplay() {
     nutritionistApp.style.display = "block";
     trainerApp.style.display = "none";
+
+    window.location.href = "#nutritionistApp";
 }
 
 function nutritionistAppSubmit() {
@@ -23,6 +25,8 @@ function nutritionistAppSubmit() {
 function trainerAppDisplay() {
     trainerApp.style.display = "block";
     nutritionistApp.style.display = "none";
+
+    window.location.href = "#trainerApp";
 }
 
 function trainerAppSubmit() {
@@ -53,43 +57,4 @@ function trainerAppSubmit() {
     var trainerStyle = document.getElementById("trainerStyle");
     var statement = document.getElementById("trainerStatement");
 }
-var specialties = document.getElementById("nutritionistSpecialties").value;
-var statement = document.getElementById("nutritionistStatement").value;
 
-console.log(firstname, lastname, email, phonenumber, availablity, rate, specialties);
-
-}
-
-function trainerAppDisplay() {
-    trainerApp.style.display = "block";
-    nutritionistApp.style.display = "none";
-}
-
-function trainerAppSubmit() {
-    var firstname = document.getElementById("trainerFirsttName").value;
-    var lastname = document.getElementById("trainerLastName").value;
-    var email = document.getElementById("trainerEmail").value;
-    var phonenumber = document.getElementById("trainerPhoneNumber").value;
-    var availablity = document.getElementById("trainerAvailability").value;
-    var rate = document.getElementById("trainerRate").value;
-    var specialties = document.getElementById("trainerSpecialties").value;
-
-    var clientTypesParent = document.getElementById("clientTypes");
-    var clientTypes = clientTypesParent.getElementsByTagName("input");
-    var clients = [];
-    for (var i = 0; i < clientTypes.length; i++) {
-        clients.push(clientTypes[i].value);
-    }
-    console.log(clients);
-
-    var trainingMedium = document.getElementById("trainingMedium");
-    var trainingMedia = trainingMedium.getElementsByTagName("input");
-    var training = [];
-    for (var i = 0; i < trainingMedia.length; i++) {
-        training.push(trainingMedia[i].value);
-    }
-    console.log(training);
-
-    var trainerStyle = document.getElementById("trainerStyle");
-    var statement = document.getElementById("trainerStatement");
-}
